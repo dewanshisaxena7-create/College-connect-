bbd5bc223a13eed4bc4aa7b2ca137268552132ea
Dev <dev@example.com>
2026-05-11 03:21:00 +0530

    migrated DB

--- college_connect/settings.py
+++ college_connect/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-
+import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-development-key-do-not-use-in-prod')
@@ -70,11 +70,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'college_connect.wsgi.application'
 
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://devanshi:nyXWgJ4XrewdN1ooRrft0IuZ8y0lr6MT@dpg-d80ej10g4nts73f6dmjg-a.singapore-postgres.render.com/my_project_db_fwlx")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, conn_health_checks=True, ssl_require=True)
 }
 
 AUTH_PASSWORD_VALIDATORS = [
